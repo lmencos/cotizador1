5214b9bec0f1998b1a6e01bec60b8c3825f5f78a
lmencos <lmencos@hotmail.com>
2020-08-31 21:59:29 -0500

    Formulario component start

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import Header from './components/Header';
+import styled from '@emotion/styled';
+
+//-------------------------------------------------------------
+//Estilos para centrar el contenido
+const Contenedor = styled.div`
+  max-width: 450px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+  border-radius: 6px;
+`;
+
+//------------------------------------------------------------------
 
 function App() {
   return (
-    <div className="App">
-      <h3>Cotizador1</h3>
+    <Contenedor className="App">
+      <p></p>
       <Header 
         title="Cotizador de seguros Lucho"
       />
-    </div>
+      <ContenedorFormulario>
+        
+      </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
+//Para dar estilo a la etiqueta header:
+const ContenedorHeader = styled.header`
+  background-color: #00A1C4;
+  padding: 3px;
+  font-weight: bold;
+  color: #FFFFFF;
+  `;
+
+const TextoHeader = styled.h2`
+  font-size: 1.8rem;
+  margin: 0;
+  font-family:'Slabo 9px', serif;
+  text-align: center;
+  `;
+
+//--------------------------------------------------------------
 const Header = ({ title }) => {
   return ( 
-    <header>
-      <h2>{ title }</h2>
-    </header>
+    <ContenedorHeader>
+      <TextoHeader>{ title }</TextoHeader>
+    </ContenedorHeader>
    );
 }
  
